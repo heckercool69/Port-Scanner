e7b3ea59f651a26dcf9c6d03ae52196e4f4cb405
heckercool69 <101942801+heckercool69@users.noreply.github.com>
2022-03-19 22:20:26 +0530

    Add files via upload

---
+++ portscanner.py
@@ -0,0 +1,28 @@
+
+import socket
+import termcolor
+
+def scan(target, ports):
+	print('\n' + ' Starting Scan for ' + str(target))
+	for port in range(1, ports):
+		scan_port(target, port)
+
+def scan_port(ipaddress, port):
+	try:
+		sock = socket.socket()
+		sock.connect((ipaddress, port))
+		print("[+] Port Opened: "+ str(port))
+		sock.close()
+	except:
+		pass
+
+
+targets = input("[*] Feed me ip address (seperate by ,): ")
+ports = int(input("[*] How many ports do you want?: "))
+
+if ',' in targets:
+	print("[*] Scanning multiple targets")
+	for ip_addr in targets.split(','):
+		scan(ip_addr.strip(' '), ports)
+else:
+	scan(targets,ports)
